1ab9e68e5a1190d604cffa57a71e6ad102d4278e
Roman Blyshchyk <roman@biorender.com>
2025-09-16 14:45:15 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MediaElement.Crash.Demo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            // Removing Shell navigation exposes the exception
+            //return new Window(new MainPage());
+
+            // Wrapping MainPage with NavigationPage allows us to catch exception
+            return new Window(new NavigationPage(new MainPage()));
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MediaElement.Crash.Demo")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MediaElement.Crash.Demo.Pages")]
